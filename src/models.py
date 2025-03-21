c2a197fe7da8520a909a5e6db6ae1c3dea806e8a
Bisal Prasad <bisalprasad2003@gmail.com>
2025-03-21 22:01:08 +0530

    added models

---
+++ src/models.py
@@ -0,0 +1,30 @@
+from langchain_groq import ChatGroq
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Initialize Groq LLM
+def get_llm():
+    llm = ChatGroq(
+        model_name="llama-3.3-70b-versatile",
+        temperature=0.9,
+        api_key= os.getenv("GROQ_API_KEY_2"),
+        max_tokens=512
+    )
+
+    return llm
+
+
+def gemini_llm():
+
+    os.environ["GOOGLE_API_KEY"] = os.getenv("GEMINI_API_KEY")
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-1.5-flash",
+        temperature=0.7,
+        max_tokens=512,
+        max_retries=2,
+    )
+
+    return llm
