4ef29b11e502b0392d10cc086e37d226155bbc43
Bisal Prasad <bisalprasad2003@gmail.com>
2025-03-21 22:00:10 +0530

    template.py and setup.py added runned and other folder created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Call_Agent_AI',
+    version= '0.0.0',
+    author= 'Team: 0101',
+    packages= find_packages(),
+    install_requires = []
+
+)
