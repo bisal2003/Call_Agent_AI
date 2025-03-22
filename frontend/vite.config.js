abc3ad22623bd31675d8730d5f95b92d24253aa3
Subrata-20 <subrata_ug_23@ece.nits.ac.in>
2025-03-22 14:50:59 +0530

    Frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
+/** @type {import('tailwindcss').Config} */
 export default defineConfig({
-  plugins: [react(),tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  theme: {
+    extend: {
+      fontFamily: {
+        square: ['Square Game', 'sans-serif'],
+      },
+    },
+  }, // Correct placement of plugins
 })
